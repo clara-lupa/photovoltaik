681f9888ef2e091aceac5472eb6c19cb87219735
clara wolff <cwo@systemli.org>
2020-12-15 13:09:20 +0100

    created read-only view and routes for PvYield

--- photovoltaik/urls.py
+++ photovoltaik/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+# from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from yields import views
+
+router = routers.DefaultRouter()
+router.register(r'pv_yield', views.PvYieldViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
+
--- yields/views.py
+++ yields/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from yields.models import PvYield
+from yields.serializers import PvYieldSerializer
 
-# Create your views here.
+class PvYieldViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = PvYield.objects.all()
+    serializer_class = PvYieldSerializer
