4f611d30602c8996f1995ae8fe7d77b39fbf2eb1
clara wolff <cwo@systemli.org>
2020-12-15 13:24:18 +0100

    added backend filters and implemented filtering by state

--- photovoltaik/settings.py
+++ photovoltaik/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'yields.apps.YieldsConfig',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
--- yields/views.py
+++ yields/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from yields.models import PvYield
 from yields.serializers import PvYieldSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 class PvYieldViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = PvYield.objects.all()
     serializer_class = PvYieldSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['state']
