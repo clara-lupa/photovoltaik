6b77f91a48bae26825aaaf93bb1a34de9587843a
clara wolff <cwo@systemli.org>
2020-12-16 17:29:45 +0100

    implemented structure for conditional logic of the view

--- yields/views.py
+++ yields/views.py
@@ -2,11 +2,31 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from yields.models import PvYield
 from yields.serializers import PvYieldSerializer
-from django_filters.rest_framework import DjangoFilterBackend
+from django.http import HttpResponse, JsonResponse
 
 
 class PvYieldViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = PvYield.objects.all()
-    serializer_class = PvYieldSerializer
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['state']
+
+    def list(self, request):
+        print(request.query_params.keys())
+        err = {}
+        if "state" in request.query_params.keys():
+            try:
+                pv_yield = PvYield.objects.get(state=request.query_params['state'])
+                serializer = PvYieldSerializer(pv_yield)
+                return JsonResponse(serializer.data, safe=False)
+            except:
+                err = {"error_message": "the state code you entered does not exist"}
+                return JsonResponse(err)
+
+        elif {"plz", "capacity"} <= request.query_params.keys():
+            try:
+                import geocoder
+                geocoder_query = "{}, Germany".format(request.query_params['plz'])
+                state = geocoder.osm(geocoder_query).osm['addr:state']
+                return JsonResponse(state, safe=False)
+            except:
+                err =  {"error_message": "invalid query format"}
+
+        return JsonResponse(err)
