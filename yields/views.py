29d7d3a116cbcf49f5080b069e2fe8691f7bf1d0
clara wolff <cwo@systemli.org>
2020-12-17 12:58:40 +0100

    refactored view, fixed style issues

--- yields/views.py
+++ yields/views.py
@@ -1,37 +1,44 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from yields.models import PvYield
-from yields.serializers import PvYieldSerializer
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
+import geocoder
 
 
 class PvYieldViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = PvYield.objects.all()
 
     def list(self, request):
-        # has to be refactored
-        print(request.query_params.keys())
-        if "state" in request.query_params.keys():
-            try:
-                pv_yield = PvYield.objects.get(state=request.query_params['state'])
-                response = {"yield": pv_yield.spec_yield, "state": pv_yield.state}
-                return JsonResponse(response)
-            except:
-                err = {"error_message": "the state code you entered does not exist"}
-                return JsonResponse(err)
 
-        elif {"plz", "capacity"} <= request.query_params.keys():
-            import geocoder
-            geocoder_query = "{}, Germany".format(request.query_params['plz'])
-            try:
-                state_full = geocoder.osm(geocoder_query).osm['addr:state']
-                pv_yield = PvYield.objects.get(state_full=state_full)
-            except:
-                err =  {"error_message": "the plz you entered does not exist"}
-                return JsonResponse(err)
-            system_yield = int(request.query_params['capacity']) * int(pv_yield.spec_yield)
-            response = {'yield': system_yield, 'state': pv_yield.state}
-            return JsonResponse(response)
+        if {'plz', 'capacity'} <= request.query_params.keys():
+            return JsonResponse(self.__pv_system_yield(request.query_params['plz'], request.query_params['capacity']))
 
-        err = {"error_message": "please enter a valid query"}
+        elif 'state' in request.query_params.keys():
+            return JsonResponse(self.__federal_state_yield(request.query_params['state']))
+
+
+        err = {'error_message': 'Please enter a valid query.'}
         return JsonResponse(err)
+
+    def __federal_state_yield(self, state):
+        try:
+            pv_yield = PvYield.objects.get(state=state)
+        except:
+            return {'error_message': 'The state code you entered does not exist.'}
+
+        return {'yield': pv_yield.spec_yield, 'state': pv_yield.state}
+
+    def __pv_system_yield(self, plz, capacity):
+        geocoder_query = '{}, Germany'.format(plz)
+        try:
+            state_full = geocoder.osm(geocoder_query).osm['addr:state']
+            pv_yield = PvYield.objects.get(state_full=state_full)
+        except:
+            return {'error_message': 'The postcode you entered does not exist.'}
+
+        try:
+            capacity_as_int = int(capacity)
+        except:
+            return {'error_message': 'The capacity value you entered is not a number.'}
+
+        system_yield = capacity_as_int * pv_yield.spec_yield
+        return {'yield': system_yield, 'state': pv_yield.state}
